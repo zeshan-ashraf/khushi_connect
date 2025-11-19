03ff2120bcbabf0c2ca16bc2024d71ba81b1bb5f
Malik Nouman <noumansam120@gmail.com>
2025-11-19 20:43:04 +0500

    add revese amount on khushi

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -31,32 +31,46 @@ class DashboardController extends Controller
             $userId = $client->id;
             
             $prevBal=Settlement::where('user_id', $userId)->whereDate('date', today()->subDay())->value('closing_bal') ?? 0;
-            $payinSuccess= Transaction::where('user_id', $userId)->where('status', 'success')->whereDate('created_at', today())->sum('amount');
-            $payoutSuccess= Payout::where('user_id', $userId)->where('status', 'success')->whereDate('created_at', today())->sum('amount');
-            $prevUsdt= Settlement::where('user_id', $userId)->whereDate('date', today())->value('usdt') ?? 0;
             $settlement=Settlement::where('user_id', $userId)->whereDate('date', today())->first();
+            
+            $epPayinAmount = $settlement->ep_payin;
+            $jcPayinAmount = $settlement->jc_payin;
+            $epPayoutAmount = $settlement->ep_payout;
+            $jcPayoutAmount = $settlement->jc_payout;
+            $reverseAmount = $settlement->reverse_amount ?? 0;
+            // if($userId == 2 || $userId == 18){
+                $payinSuccess= $epPayinAmount + $jcPayinAmount;
+            // }else{
+                // $payinSuccess= $jcPayinAmount;
+            // }
+            $payoutSuccess= $epPayoutAmount + $jcPayoutAmount;
+            $prevUsdt= $settlement->usdt;
             $payinFee=$client->payin_fee;
             $payoutFee=$client->payout_fee;
-            $unsettletdAmount=$prevBal + $payinSuccess - ($payinSuccess*$payinFee + $payoutSuccess + $payoutSuccess*$payoutFee + $prevUsdt);
-            $reverseAmount = $settlement->reverse_amount ?? 0;
             //getUnsettlement
-            $data[] = [
+            // if ($userId == 2) {
+            //     $payinSuccess = $epPayinAmount;
+            // } 
+            $unsettletdAmount=$prevBal + $payinSuccess - ($payinSuccess*$payinFee + $payoutSuccess + $payoutSuccess*$payoutFee + $prevUsdt);
+            $assignedAmount=Setting::where('user_id',$userId)->select('jazzcash','easypaisa','payout_balance')->first();
+            $balance= $unsettletdAmount - $assignedAmount->payout_balance ;
+    
+            $data[] = [ 
                 'user' => $client,
                 'prev_balance' => $prevBal,
-                'jc_payin' => payinJCFunc($userId),
-                'ep_payin' => payinEPFunc($userId),
+                'jc_payin' => $jcPayinAmount,
+                'ep_payin' => $epPayinAmount,
                 'reverse_amount' => $reverseAmount,
                 'total_payin' => $payinSuccess,
-                'jc_payout' => payoutJCFunc($userId),
-                'ep_payout' => payoutEPFunc($userId),
+                'jc_payout' => $jcPayoutAmount,
+                'ep_payout' => $epPayoutAmount,
                 'total_payout' => $payoutSuccess,
                 'prev_usdt' => $prevUsdt,
                 'unsettled_amount' => $unsettletdAmount,
-                'unsettled_amount_balance' => getUnsettlement($userId),
+                'unsettled_amount_balance' => $balance,
                 'assigned_amount' => Setting::where('user_id', $userId)->first(),
                 'setting' => Setting::where('user_id', $userId)->first(),
             ];
-            
         }
         $totals = [
             'prev_balance' => 0,
