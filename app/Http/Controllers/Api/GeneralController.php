2f165a957166337ba9e63dce38efca3db31930a9
Malik Nouman <noumansam120@gmail.com>
2025-12-12 03:57:21 +0500

    change in setllemtn api

--- app/Http/Controllers/Api/GeneralController.php
+++ app/Http/Controllers/Api/GeneralController.php
@@ -214,7 +214,7 @@ class GeneralController extends Controller
     {
         $activeUserIds = User::where('active', 1)->pluck('id');
         $settlementData = Settlement::whereIn('user_id', $activeUserIds)
-            ->where('date', '>=', now())
+            ->whereDate('date', Carbon::today())
             ->orderBy('date', 'desc')
             ->get();
         $settingData=Setting::whereIn('user_id', $activeUserIds)->get();
