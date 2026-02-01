0959c63908c404686f8758c375100b4c4a5f4939
Malik Nouman <noumansam120@gmail.com>
2026-02-01 14:25:03 +0500

    updateBalance change button added

--- app/Http/Controllers/Api/PayoutController.php
+++ app/Http/Controllers/Api/PayoutController.php
@@ -378,7 +378,7 @@ class PayoutController extends Controller
                     //    'response' => $setting,
                     //    'user payout fee' => $user,
                     //]);
-                    if ($setting && $user->per_payout_fee && $setting->auto ==1) {
+                    if ($setting && $user->per_payout_fee) {
                         $rate = $user->per_payout_fee;
                         $amount = $request->amount * $rate;
                     
