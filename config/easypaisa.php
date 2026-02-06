9133398f6a9a6b843151a2339b2122100bd560e1
Malik Nouman <noumansam120@gmail.com>
2026-02-06 17:44:49 +0500

    add substore data

--- config/easypaisa.php
+++ config/easypaisa.php
@@ -15,7 +15,18 @@ return [
     'prod_password'=>env('EASYPAISA_PRODUCTION_PASSWORD'),
     'prod_storeid'=> env('EASYPAISA_PRODUCTION_STOREID'),
     
+    'prod_move_username'=> env('EASYPAISA_PRODUCTION_MOVE_USERNAME'),
+    'prod_move_password'=>env('EASYPAISA_PRODUCTION_MOVE_PASSWORD'),
+    'prod_move_storeid'=> env('EASYPAISA_PRODUCTION_MOVE_STOREID'),
 
+    'prod_young_username'=> env('EASYPAISA_PRODUCTION_YOUNG_USERNAME'),
+    'prod_young_password'=>env('EASYPAISA_PRODUCTION_YOUNG_PASSWORD'),
+    'prod_young_storeid'=> env('EASYPAISA_PRODUCTION_YOUNG_STOREID'),
+
+    'prod_desk_username'=> env('EASYPAISA_PRODUCTION_DESK_USERNAME'),
+    'prod_desk_password'=>env('EASYPAISA_PRODUCTION_DESK_PASSWORD'),
+    'prod_desk_storeid'=> env('EASYPAISA_PRODUCTION_DESK_STOREID'),
+    
     'prod_hashkey'=> env('EASYPAISA_PRODUCTION_HASHKEY'),
     'prod_url'=> env('EASYPAISA_PRODUCTION_URL'),
 
