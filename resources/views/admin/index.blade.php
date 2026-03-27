76a527694f2536cfa00fde331d26fbe0f5e91141
Malik Nouman <noumansam120@gmail.com>
2026-03-27 15:34:46 +0500

    add manual button on dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -200,7 +200,6 @@
                                                         <th colspan="3">Wallet</th>
                                                         @if(auth()->user()->user_role == "Super Admin" || auth()->user()->user_role == "Manager" || auth()->user()->user_role == "Client")
                                                         <th colspan="3" rowspan="3">Balance</th>
-                                                        <th colspan="3" rowspan="2">USDT & Wallet</th>
                                                         @endif
                                                         @if(auth()->user()->user_role == "Super Admin" || auth()->user()->user_role == "Manager")
                                                         <th colspan="3" rowspan="2">USDT & Wallet</th>
