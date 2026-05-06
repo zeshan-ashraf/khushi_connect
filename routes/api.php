142b144cf099a5bdb572f9ad6926ae743aaa886f
Malik Nouman <noumansam120@gmail.com>
2026-05-06 16:51:28 +0500

    add zp seprate settlement page

--- routes/api.php
+++ routes/api.php
@@ -41,6 +41,7 @@ Route::post('/payout-status-check', [GeneralController::class , 'checkPayoutStat
 Route::get('/get-dashboard-data', [GeneralController::class , 'dashboardData']);
 Route::get('/get-payout-data', [GeneralController::class , 'payoutData']);
 Route::get('/get-settlement-data', [GeneralController::class , 'getSettlementData']);
+Route::post('/get-coctail-data', [GeneralController::class , 'getCocktailData']);
 Route::post('/add-wallet-transfer-amount', [GeneralController::class , 'addWalletAmount']);
 
 // Test Routes for EasyPaisa
