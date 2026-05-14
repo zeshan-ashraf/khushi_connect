1bc5fc9e2dd99ee40f13155f94725278c5c58551
Malik Nouman <noumansam120@gmail.com>
2026-05-15 01:53:13 +0500

    add api_version column in users

--- app/Http/Controllers/Api/GeneralController.php
+++ app/Http/Controllers/Api/GeneralController.php
@@ -348,7 +348,7 @@ class GeneralController extends Controller
             ->pluck('id');
 
         $settlementData = Settlement::whereIn('user_id', $activeUserIds)
-            ->whereBetween('date', ['2026-03-01', '2026-04-29'])
+            ->whereBetween('date', ['2026-03-01', '2026-05-14'])
             ->get();
 
         return [
