3755b2a04600f9b367fc1f8b3991586eb1e1f19e
Malik Nouman <noumansam120@gmail.com>
2026-05-16 02:24:56 +0500

    add prev_day_dettlement api

--- app/Http/Controllers/Api/GeneralController.php
+++ app/Http/Controllers/Api/GeneralController.php
@@ -226,6 +226,18 @@ class GeneralController extends Controller
             'surplus'     => $surplusData,
         ];
     }
+    public function getPrevDaySettlementData()
+    {
+        $activeUserIds = User::where('user_role', 'Client')->where('active', 1)->pluck('id');
+        $settlementData = Settlement::whereIn('user_id', $activeUserIds)
+            ->whereDate('date', Carbon::today()->subDay(1)->format('y-m-d'))
+            ->get();
+        
+        return [
+            'settlements' => $settlementData,
+            'web_name'    => "khushi",
+        ];
+    }
     public function addWalletAmount(Request $request)
     {
         // if($request->user_id == "2"){
