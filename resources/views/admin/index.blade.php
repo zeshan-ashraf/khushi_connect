df831ab491d32ab8b58d3d60a195d83e8c862762
Malik Nouman <noumansam120@gmail.com>
2026-05-28 18:06:05 +0500

    ok user on top

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -260,7 +260,7 @@
                                                             @if(auth()->user()->user_role == "Super Admin" || auth()->user()->user_role == "Manager" || auth()->user()->id == $user->id)
                                                             <td class="bg-warning">{{ number_format(round($item['unsettled_amount_balance'], 0)) }}</td>
                                                             @endif
-                                                            @if(auth()->user()->user_role == "Super Admin" || auth()->user()->user_role == "Manager" || auth()->user()->id == 9 || auth()->user()->id == 10 || auth()->user()->id == 13)
+                                                            @if(auth()->user()->user_role == "Super Admin" || auth()->user()->user_role == "Manager" || auth()->user()->id == 9 || auth()->user()->id == 10 || auth()->user()->id == 12 || auth()->user()->id == 13)
                                                             <td class="bg-warning">
                                                                 <div class="d-flex justify-content-start">
                                                                         <a class="dropdown-item btn btn-primary w-auto open_modal me-1" 
