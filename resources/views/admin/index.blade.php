0953aa43a17f1223cd58c04777af39c938a2e08a
Malik Nouman <noumansam120@gmail.com>
2026-06-10 15:49:50 +0500

    add payout company settings

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -160,7 +160,7 @@
                                                         <th colspan="2"><a data-target="#attributeModal" class="btn btn-primary waves-effect waves-float waves-light open_modal" data-url="{{route('admin.setting.modal_sec')}}">Add Amount</a></th>
                                                         @foreach($payout_setting as $item)
                                                         <th>
-                                                            <h4>{{ $item->type }}</h4>
+                                                            <h6>{{ $item->type }}</h6>
                                                             <div class="form-check form-switch">
                                                                 <input 
                                                                     class="form-check-input toggle-switch-payout" 
