d8f030739ebfc2987cf529c5ba683ef9bcc9dff7
Malik Nouman <noumansam120@gmail.com>
2026-06-29 12:55:07 +0500

    ok reversal 100 percent

--- app/Http/Controllers/Api/GeneralController.php
+++ app/Http/Controllers/Api/GeneralController.php
@@ -249,7 +249,11 @@ class GeneralController extends Controller
         $userId = null;
 
         if ($request->from_store_name == "Monotech") {
-            $userId = $request->user_id == "2" ? 4 : ($request->user_id == "4" ? 3 : null);
+            // $userId = $request->user_id == "2" ? 4 : ($request->user_id == "4" ? 3 : null);
+
+            $userId = $request->user_id == "2" ? 4
+                    : ($request->user_id == "4" ? 3
+                    : ($request->user_id == "25" ? 12 : null));
         } else {
             $userId = $request->user_id == "19" ? 4 : ($request->user_id == "4" ? 36 : null);
         }
