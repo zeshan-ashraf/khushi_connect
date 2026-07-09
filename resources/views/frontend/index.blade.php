d9df93830b1f79a913b6b679926d5c58931488a9
zeshan-ashraf <malikzeshanashraf@gmail.com>
2026-07-09 16:32:52 +0500

    ops dashboar

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -2,9 +2,6 @@
 @section('title','Home')
 @section('content')
 
-<!-- Server Upgrade Notification -->
-@include('frontend.component.server-upgrade-notification')
-
 <!-- slider-area -->
 <section id="home" class="slider-area fix p-relative">
     <div class="slider-circal"></div>
